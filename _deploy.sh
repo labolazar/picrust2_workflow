e8fbd36bfaae68ff643c654abad678bded0581e2
karinevilleneuve <karine.villeneuve12@gmail.com>
2024-07-22 17:21:38 -0400

    updated folder to build book

--- _deploy.sh
+++ _deploy.sh
@@ -5,8 +5,8 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "villeneuve.karine.5@ens.uqam.ca"
-git config --global user.name "YKarine Villeneuve"
+git config --global user.email "xie@yihui.name"
+git config --global user.name "Yihui Xie"
 
 git clone -b gh-pages https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git book-output
 cd book-output
